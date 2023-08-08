f0e18e3d0cda1f39cf55370cfd2a9c495c01210b
Pedro Henrique <p13dr0h@gmail.com>
2023-08-08 11:51:11 -0300

    chore: setup project

---
+++ src/main/java/br/com/ph/gestao_funcionario/domain/model/Funcionario.java
@@ -0,0 +1,5 @@
+package br.com.ph.gestao_funcionario.domain.model;
+
+public class Funcionario {
+
+}
---
+++ src/test/java/br/com/ph/gestao_funcionario/GestaoFuncionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ph.gestao_funcionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFuncionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

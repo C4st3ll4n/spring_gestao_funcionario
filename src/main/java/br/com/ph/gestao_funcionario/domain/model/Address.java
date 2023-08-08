f29192ed575daeea56afacb92c4ddfa6fc78824d
Pedro Henrique <p13dr0h@gmail.com>
2023-08-08 12:26:42 -0300

    feat: add getters and setters

--- src/main/java/br/com/ph/gestao_funcionario/domain/model/Address.java
+++ src/main/java/br/com/ph/gestao_funcionario/domain/model/Address.java
@@ -21,4 +21,44 @@ public class Address {
     private String city;
 
     private String postalCode;
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getNeighborhood() {
+        return neighborhood;
+    }
+
+    public void setNeighborhood(String neighborhood) {
+        this.neighborhood = neighborhood;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 }
--- src/main/java/br/com/ph/gestao_funcionario/domain/model/Employee.java
+++ src/main/java/br/com/ph/gestao_funcionario/domain/model/Employee.java
@@ -19,4 +19,44 @@ public class Employee {
     private String role;
     @Column
     private Double salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
 }

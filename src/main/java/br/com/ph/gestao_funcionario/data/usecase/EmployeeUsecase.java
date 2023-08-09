968956ba40548098d292de423c17fc563047aa7a
Pedro Henrique <p13dr0h@gmail.com>
2023-08-09 15:11:53 -0300

    refactor: remove employee usecase

--- src/main/java/br/com/ph/gestao_funcionario/data/usecase/EmployeeUsecase.java
+++
@@ -1,9 +0,0 @@
-package br.com.ph.gestao_funcionario.data.usecase;
-
-import br.com.ph.gestao_funcionario.domain.model.EmployeeModel;
-import br.com.ph.gestao_funcionario.domain.usecase.CreateUser;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeUsecase extends CreateUser, CrudRepository<EmployeeModel, String> {
-    EmployeeModel findById(long id);
-}
